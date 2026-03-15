996b2b23543a30d4b747cbc54a296520c1116f58
iloantsa <ton@email.com>
2026-03-15 19:28:17 +0100

    week two assignment secon ckeck

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,24 +1,29 @@
-// In your blank JavaScript file, declare three variables that hold references to the input, button, and list elements.
+// select elements from the DOM
+const inputElement = document.querySelector("#favchap");
+const buttonElement = document.querySelector("button");
+const listElement = document.querySelector("#list");
 
-
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('______');
-
-// create a li element that will hold each entry's chapter title and an associated delete button.
-
-const li = document.createElement('li');
-// create a delete button.
-
-const deleteButton = document.createElement('button');
-
-// Populate the li element variable's textContent or innerHTML with the input value.
-li.textContent = input.value;
-
-// Set the delete button's textContent to ❌.
-deleteButton.textContent = '❌';
-// Append the delete button to the li element.
-li.append(deleteButton);
-// Append the li element variable to the unordered list in your HTML.
-
-list.append(li);
+// wait for button clicks
+buttonElement.addEventListener("click", function () {
+    // Check if the user entered something
+    if (inputElement.value != "") {
+        // create list item and give it the value of the input
+        const li = document.createElement("li");
+        li.textContent = inputElement.value;
+        // create a button and add a click event listener
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        deleteBtn.addEventListener("click", function () {
+            listElement.removeChild(li);
+            inputElement.focus();
+        });
+        // add the button to the list item
+        li.appendChild(deleteBtn);
+        // OUTPUT: finally display the completed list item to the unordered list
+        listElement.appendChild(li);
+        // clear the user input field
+        inputElement.value = "";
+    }
+    // focus the user back to the input field
+    inputElement.focus();
+});
